45e3554869cfe75f08a7c5088fad6bbb71468291
LeonilsonMorais <115759629+LeonilsonMorais@users.noreply.github.com>
2022-11-22 23:13:37 -0300

    Update Exercicio1.java

--- CursoJavaExercicios/src/fundamentos/Exercicio1.java
+++ CursoJavaExercicios/src/fundamentos/Exercicio1.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Exercicio1 {
 	
-	/** 1. Criar um programa que leia a temperatura em Celsius e converta para Fahrenheit. */
+	/** 1. Criar um programa que leia a temperatura em Fahrenheit e converta para Celsius. */
 	
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Digite a temperatura em Celsius: ");
-		double celsius = scanner.nextDouble();
+		System.out.println("Digite a temperatura em Fahrenheit: ");
+		double fahrenheit = scanner.nextDouble();
 		
-		double conversao = ( celsius - 32 ) / 1.8;
+		double conversao = ( fahrenheit - 32 ) / 1.8;
 		
-		System.out.print("Valor em Farenheit: " + conversao);
+		System.out.print("Valor em Celsius: " + conversao);
 		
 		scanner.close();
 	}
